f4c59cf770e3549a85fc5fc8b2596f4b40909a8d
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-05-01 02:51:19 +0200

    Server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var httpServer = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-  res.sendfile('public/index.html');
+    res.sendfile('public/index.html');
 });
 
 httpServer.listen(9003, function() {
-  console.log('listening on *:9003');
+    console.log('listening on *:9003');
 });
 
 
@@ -16,116 +16,118 @@ var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 9002 }),
     users = {};
 
-wss.on('connection', function (connection) {
-  connection.on('message', function (message) {
-    var data;
+wss.on('connection', function(connection) {
+    connection.on('message', function(message) {
+        var data;
 
-    try {
-      data = JSON.parse(message);
-    } catch (e) {
-      console.log("Error parsing JSON");
-      data = {};
-    }
-
-    switch (data.type) {
-      case "login":
-        console.log("User logged in as", data.source);
-        if (users[data.source]) {
-          //username already exixsts, login not possible
-          //sendTo connection with type and success
-          //TODO
-
-        } else {
-          users[data.source] = connection;
-          connection.source = data.source;
-          sendTo(connection, {
-            type: "login",
-            success: true
-          });
+        try {
+            data = JSON.parse(message);
+        } catch (e) {
+            console.log("Error parsing JSON");
+            data = {};
         }
 
-        break;
-      case "offer":
-        console.log("Sending offer to", data.target);
-        var conn = users[data.target];
+        switch (data.type) {
+            case "login":
+                console.log("User logged in as", data.source);
+                if (users[data.source]) {
+                    sendTo(connection, {
+                        type: "login",
+                        success: false
+                    });
+                } else {
+                    users[data.source] = connection;
+                    connection.source = data.source;
+                    sendTo(connection, {
+                        type: "login",
+                        success: true
+                    });
+                }
+                break;
 
-        if (conn != null) {
-          connection.target = data.target;
-          //send offer to conn
-          //sendTo conn with type, offer and source
-          //TODO
+            case "offer":
+                console.log("Sending offer to", data.target);
+                var conn = users[data.target];
+
+                if (conn != null) {
+                    connection.target = data.target;
+                    sendTo(conn, {
+                        type: "offer",
+                        offer: data.offer,
+                        source: connection.source
+                    });
+                }
+                break;
+
+            case "answer":
+                console.log("Sending answer to", data.target);
+                var conn = users[data.target];
+
+                if (conn != null) {
+                    connection.target = data.target;
+                    sendTo(conn, {
+                        type: "answer",
+                        answer: data.answer
+                    });
+                }
+                break;
+
+            case "candidate":
+                console.log("Sending candidate to", data.target);
+                var conn = users[data.target];
+
+                if (conn != null) {
+                    sendTo(conn, {
+                        type: "candidate",
+                        candidate: data.candidate
+                    });
+                }
+                break;
+
+            case "leave":
+                console.log("Disconnecting user from", data.target);
+                var conn = users[data.target];
+                conn.target = null;
+
+                if (conn != null) {
+                    sendTo(conn, {
+                        type: "leave"
+                    });
+                }
+                break;
+
+            default:
+                sendTo(connection, {
+                    type: "error",
+                    message: "Unrecognized command: " + data.type
+                });
+                break;
         }
+    });
 
-        break;
-      case "answer":
-        console.log("Sending answer to", data.target);
-        var conn = users[data.target];
+    connection.on('close', function() {
+        if (connection.source) {
+            delete users[connection.source];
 
-        if (conn != null) {
-          connection.target = data.target;
-          //send answer to conn
-          //sendTo conn with type and answer
-          //TODO
+            if (connection.target) {
+                console.log("Disconnecting user from", connection.target);
+                var conn = users[connection.target];
+                conn.target = null;
+
+                if (conn != null) {
+                    sendTo(conn, {
+                        type: "leave"
+                    });
+                }
+            }
         }
-
-        break;
-      case "candidate":
-        console.log("Sending candidate to", data.target);
-        var conn = users[data.target];
-
-        if (conn != null) {
-          sendTo(conn, {
-            type: "candidate",
-            candidate: data.candidate
-          });
-        }
-
-        break;
-      case "leave":
-        console.log("Disconnecting user from", data.target);
-        var conn = users[data.target];
-        conn.target = null;
-
-        if (conn != null) {
-          sendTo(conn, {
-            type: "leave"
-          });
-        }
-
-        break;
-      default:
-        sendTo(connection, {
-          type: "error",
-          message: "Unrecognized command: " + data.type
-        });
-
-        break;
-    }
-  });
-
-  connection.on('close', function () {
-    if (connection.source) {
-      delete users[connection.source];
-
-      if (connection.target) {
-        console.log("Disconnecting user from", connection.target);
-        var conn = users[connection.target];
-        conn.target = null;
-
-        if (conn != null) {
-          sendTo(conn, {
-            type: "leave"
-          });
-        }
-      }
-    }
-  });
+    });
 });
 
 function sendTo(conn, message) {
-  conn.send(JSON.stringify(message));
+    conn.send(JSON.stringify(message));
 }
 
-wss.on('listening', function () {
+wss.on('listening', function() {
     console.log("Server started...");
 });
